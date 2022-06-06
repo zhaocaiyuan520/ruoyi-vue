3eb5fca0cd24c433f0f66faca071b5345bb25bfa
zhaocy <zhao_caiyuan@163.com>
2022-06-06 09:07:44 +0800

    打开验证码

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysLoginService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysLoginService.java
@@ -61,10 +61,10 @@ public class SysLoginService
     {
         boolean captchaOnOff = configService.selectCaptchaOnOff();
         // 验证码开关  验证码
-//        if (captchaOnOff)
-//        {
-//            validateCaptcha(username, code, uuid);
-//        }
+        if (captchaOnOff)
+        {
+            validateCaptcha(username, code, uuid);
+        }
         // 用户验证
         Authentication authentication = null;
         try
